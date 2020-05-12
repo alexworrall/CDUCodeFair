2b3f2a555e1dca2c9387c20369ea21a0721f894c
alexworrall <50971302+alexworrall@users.noreply.github.com>
2020-05-12 11:12:56 +0930

    Added all enquiry form types

--- js/splitLayout.js
+++ js/splitLayout.js
@@ -9,6 +9,8 @@ $(document).ready(function() {
 		{ flexBasis: "34.75%"}, 500);
 	  $("#accordian .details > div").show(500);
 	  $(".registeringContactForm").hide(500);
+	  $(".businessContactForm").hide(500);
+	  $(".generalContactForm").hide(500);
 	  $(".mainContactForm").show(500);
 	  //$("#accordian .contactForm > div").hide(500);
 	  $('.contactForm h1').removeClass('checked');
@@ -33,7 +35,41 @@ $(document).ready(function() {
 	  { flexBasis: "6%" }, 500);
 	$("#accordian .contactForm").animate(
 	  { flexBasis: "70.55%" }, 500);
-	$("#accordian > .contactForm > div").show(500);
+	$("#accordian > .contactForm > .registeringContactForm").show(500);
+	$("#accordian > .details > div").hide(500);
+	$("#accordian > .contactForm > mapsContainer").hide(500);
+	document.getElementById("detailsHeading").innerHTML = "Back >";
+	$('.details h1').removeClass('checked');
+	$('.contactForm h1').removeClass('small');
+	$(".mainContactForm").hide(500);
+  }
+
+  function radioBusinessClick(){
+	$('.contactForm h1').addClass('checked'); 
+	$('.details h1').addClass('small');      
+	$("#accordian .details").animate(
+	  { flexBasis: "6%" }, 500);
+	$("#accordian .contactForm").animate(
+	  { flexBasis: "70.55%" }, 500);
+	//$("#accordian > .contactForm > div").show(500);
+	$("#accordian > .contactForm > .businessContactForm").show(500);
+	$("#accordian > .details > div").hide(500);
+	$("#accordian > .contactForm > mapsContainer").hide(500);
+	document.getElementById("detailsHeading").innerHTML = "Back >";
+	$('.details h1').removeClass('checked');
+	$('.contactForm h1').removeClass('small');
+	$(".mainContactForm").hide(500);
+  }
+
+  function radioGeneralClick(){
+	$('.contactForm h1').addClass('checked'); 
+	$('.details h1').addClass('small');      
+	$("#accordian .details").animate(
+	  { flexBasis: "6%" }, 500);
+	$("#accordian .contactForm").animate(
+	  { flexBasis: "70.55%" }, 500);
+	//$("#accordian > .contactForm > div").show(500);
+	$("#accordian > .contactForm > .generalContactForm").show(500);
 	$("#accordian > .details > div").hide(500);
 	$("#accordian > .contactForm > mapsContainer").hide(500);
 	document.getElementById("detailsHeading").innerHTML = "Back >";
